ee5a29f30a5d3c37d1922b67c551329eb3d7d155
Sanjay Kandula <sanjaykandula3@gmail.com>
2024-02-07 22:31:31 +0530

    First Commit

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 mongoose
-  .connect('mongodb://localhost:27017', {
-    dbName: 'url-shortner',
+  .connect('mongodb+srv://sanjay:sanjay@cluster0.fjcbkym.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
